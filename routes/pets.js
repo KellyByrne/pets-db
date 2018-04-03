20def97dde290e2ca4d6f0cfd24ea0f97edfb7d9
Kelly Byrne <kellyannbyrne@utexas.edu>
2018-04-02 21:39:56 -0400

    first commit

---
+++ routes/pets.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var petSchema = new Schema({
+
+});
+
+var Pet = monggose.model('Project', petSchema);
+
+// Get All Pets
+router.get('/', function(req, res, next) {
+  Pet.find({})
+    .then(function(result){
+      res.json(result)
+    })
+    .catch(function(err){
+      res.json(err)
+    })  
+});
+
+// Get One Pet
+router.get('/:id', function(req, res, next) {
+  Pet.findOne({_id: req.params.id})
+    .then(function(result){
+      res.json(result)
+    })
+    .catch(function(err){
+      res.json(err)
+    })
+});
+
+// Create New Pet
+router.post('/', function(req, res, next){
+  const animal = req.body.animal;
+  Pet.create(animal)
+    .then(function(result){
+      res.json(result)
+    })
+    .catch(function(err){
+      res.json(err)
+    })
+});
+
+//Edit One Pet
+router.post('/:id', function(req, res, next){
+  Pet.findOneAndUpdate({_id: req.params.id}, req.body )
+  .then(function(result){
+    res.json(result)
+  })
+  .catch(function(err){
+    res.json(err)
+  })
+});
+
+//Delete One Pet
+router.post('/:id', function(req, res, next){
+  Pet.deleteOne({_id: req.params.id})
+  .then(function(result){
+    res.json(result)
+  })
+  .catch(function(err){
+    res.json(err)
+  })
+});
+
+module.exports = router;
