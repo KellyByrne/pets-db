39bba5399a0a69ac9ab6eb12d492bcfa68c0a7bd
Kelly Byrne <kellyannbyrne@utexas.edu>
2018-04-02 22:10:14 -0400

    third

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+app.use('/api/pets', pets);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
